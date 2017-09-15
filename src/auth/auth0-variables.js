072f3374f564267ac92ab309605100fab1c6dbec
Ling Su <ling-su@northwestern.edu>
2017-09-15 10:12:48 -0500

    archive AuthO solution

---
+++ src/auth/auth0-variables.js
@@ -0,0 +1,6 @@
+export const AUTH_CONFIG = {
+  clientId: '5Z1Mvp1JRgC1UI1m22DzfJkHLDk3eC9v',
+  domain: 'mslingsu.auth0.com',
+  // callbackUrl: 'http://foo.bar.northwestern.edu:8080/callback'
+  callbackUrl: 'http://localhost:8080/callback'
+}
